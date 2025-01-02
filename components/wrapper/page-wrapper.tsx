8a636ad748760866ddd09eb43a4bf71b4bf5acf2
Ken Ny <kennynguyen1631984@gmail.com>
2025-01-03 02:56:47 +0700

    refactor: add responsive margin to main element in PageWrapper component

--- components/wrapper/page-wrapper.tsx
+++ components/wrapper/page-wrapper.tsx
@@ -6,7 +6,9 @@ export default function PageWrapper({
   return (
     <>
       {/* <Header /> */}
-      <main>{children}</main>
+      <main className="md:mt-[2rem] lg:mt-[3rem] xl:mt-[4rem] 2xl:mt-[5rem]">
+        {children}
+      </main>
       {/* <Footer /> */}
     </>
   );
