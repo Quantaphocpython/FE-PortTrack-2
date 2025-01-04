9f31319c7433a67685a6a485eda5590e251079f2
Quantaphocpython <kaitoukido0204@gmail.com>
2025-01-04 18:16:49 +0700

    :sparkles:[feat]: connect wallet and init chain

---
+++ configs/WalletConfig.ts
@@ -0,0 +1,59 @@
+import { getDefaultConfig, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { ledgerWallet, trustWallet } from '@rainbow-me/rainbowkit/wallets';
+import { http } from 'wagmi';
+import { defineChain } from 'viem';
+
+const AssetHubWestend = defineChain({
+  id: 420420421,
+  name: 'AssetHub',
+  nativeCurrency: {
+    name: 'Westie',
+    symbol: 'WST',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://westend-asset-hub-eth-rpc.polkadot.io'],
+    },
+  },
+  testnet: true,
+});
+
+const Kitchensink = defineChain({
+  id: 420420420,
+  name: 'Kitchensink',
+  nativeCurrency: {
+    name: 'Dev',
+    symbol: 'DEV',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://localhost:8545'],
+    },
+  },
+  testnet: true,
+});
+
+const { wallets } = getDefaultWallets();
+
+const walletConfig = getDefaultConfig({
+  appName: process.env.NEXT_PUBLIC_APP_NAME || '',
+  projectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || '',
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [trustWallet, ledgerWallet],
+    },
+  ],
+  chains: [AssetHubWestend, Kitchensink],
+  transports: {
+    [AssetHubWestend.id]: http(),
+    [Kitchensink.id]: http(),
+  },
+  ssr: true,
+});
+
+export default walletConfig;
+export { AssetHubWestend, Kitchensink };
---
+++ provider/WalletProvider.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import walletConfig from '@/configs/WalletConfig';
+import {
+  RainbowKitProvider,
+  darkTheme,
+  lightTheme,
+} from '@rainbow-me/rainbowkit';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useTheme } from 'next-themes';
+import React, { useEffect, useState } from 'react';
+import { WagmiProvider } from 'wagmi';
+import { AssetHubWestend as network } from '@/configs/WalletConfig';
+
+const queryClient = new QueryClient();
+
+type Props = React.ReactNode;
+
+export default function ContextProvider({ children }: { children: Props }) {
+  const [mounted, setMounted] = React.useState(false);
+  const { theme } = useTheme();
+  const [rainbowKitTheme, setRainbowKitTheme] = useState(
+    darkTheme({
+      accentColor: '#ff8800',
+      accentColorForeground: 'white',
+      borderRadius: 'none',
+    })
+  );
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    const newTheme =
+      theme === 'dark'
+        ? darkTheme({
+            accentColor: '#ff8800',
+            accentColorForeground: 'white',
+            borderRadius: 'none',
+          })
+        : lightTheme({
+            accentColor: '#ff8800',
+            accentColorForeground: 'white',
+            borderRadius: 'none',
+          });
+
+    setRainbowKitTheme(newTheme);
+  }, [theme]);
+
+  return (
+    <WagmiProvider config={walletConfig}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          initialChain={network}
+          showRecentTransactions={true}
+          theme={rainbowKitTheme}
+          locale="en-US"
+        >
+          {mounted && children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
